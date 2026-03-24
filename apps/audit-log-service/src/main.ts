4128e467436f51cba1d3aa14595e567f50bf7df4
abcdefghuy <nguyensanghuypy@gmail.com>
2026-03-24 12:39:34 +0700

    fix: add gateway exception in audit log

--- apps/audit-log-service/src/main.ts
+++ apps/audit-log-service/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AuditLogModule } from './audit-log.module';
+import { HttpToRpcExceptionFilter, RpcDomainExceptionFilter } from '@app/common/filters';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -16,6 +17,10 @@ async function bootstrap() {
       },
     },
   );
+  app.useGlobalFilters(
+      new HttpToRpcExceptionFilter(),
+      new RpcDomainExceptionFilter(),
+    );
   await app.listen();
   console.log('Audit Log Microservice is listening');
 }
